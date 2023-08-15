5e366e9ae45e86370064ec1f1df8cf18c185706c
oksanaivashko <oksivashko13@gmail.com>
2023-08-15 13:44:55 -0500

    testing ec2

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 module "demo_ec2_dev" {
   source = "./modules/ec2"
+
+  ami = var.ami
+  ec2_type = var.ec2_type
 }
 
-# --- Variables ---
-variable "ami" {
-  type = string
-  default = "ami-04e35eeae7a7c5883"
-}
-variable "ec2_type" {
-  type = string
-  default = "t2.micro"
-}
+
+
+## --- Variables ---
+#variable "ami" {
+#  type = string
+#  default = "ami-04e35eeae7a7c5883"
+#}
+#variable "ec2_type" {
+#  type = string
+#  default = "t2.micro"
+#}
