75a9cba553e381eb100ec43b03bb5336b9a9722d
oksanaivashko <oksivashko13@gmail.com>
2023-08-16 17:16:57 -0500

    added vpc module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,4 +19,7 @@
 
 module "demo_dev_vpc" {
   source = "../../modules/vpc"
+  cidr_block    = "10.0.0.0/16"
+  demo_vpc_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  demo_vpc_az   = "us-west-2a"
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
   description = "Id of the created VPC"
-  value       = module.demo_vpc.vpc_id
+  value       = module.demo_dev_vpc.vpc_id
 }
 
 output "subnet_ids" {
   description = "List of Ids of the created subnets"
-  value       = module.demo_vpc.subnet_ids
+  value       = module.demo_dev_vpc.subnet_ids
 }
