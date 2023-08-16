10c795f6bffc570dad1d583ec414d697555449ed
oksanaivashko <oksivashko13@gmail.com>
2023-08-16 17:19:47 -0500

    testing vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,11 +10,3 @@ resource "aws_subnet" "demo_vpc_subnets" {
   availability_zone      = var.demo_vpc_az 
   map_public_ip_on_launch = true
 }
-
-#output "vpc_id" {
-#  value = module.demo_vpc.vpc_id
-#}
-#
-#output "subnet_ids" {
-#  value = module.demo_vpc.subnet_ids
-#}
--- modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "Id of the created VPC"
-  value       = module.demo_dev_vpc.vpc_id
-}
-
-output "subnet_ids" {
-  description = "List of Ids of the created subnets"
-  value       = module.demo_dev_vpc.subnet_ids
-}
