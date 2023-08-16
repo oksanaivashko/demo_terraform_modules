657f758e7e377e3b881406afd10899fba5a04c57
oksanaivashko <oksivashko13@gmail.com>
2023-08-16 17:00:53 -0500

    adding vpc module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,22 @@
-module "demo_ec2_dev" {
-  source = "../../modules/ec2"
+# --- Terraform module to create ec2 Insance ---
 
-  ami = var.ami
-  ec2_type = var.ec2_type
-  ec2_name = var.ec2_name
+#module "demo_ec2_dev" {
+#  source = "../../modules/ec2"
+#
+#  ami = var.ami
+#  ec2_type = var.ec2_type
+#  ec2_name = var.ec2_name
+#}
+
+
+# --- Terraform module to create sg ---
+
+#module "demo_dev_sg" {
+#  source = "../../modules/sg"
+#}
+
+# --- Terraform module to vpc ---
+
+module "demo_dev_vpc" {
+  source = "../../modules/vpc"
 }
-
