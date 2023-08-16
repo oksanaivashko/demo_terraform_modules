84f47ec68c8be419057d70a97bb6fff8ebf81f5d
oksanaivashko <oksivashko13@gmail.com>
2023-08-15 19:29:49 -0500

    adding ec2 to prod

--- prod/main.tf
+++ prod/main.tf
@@ -1,7 +1,3 @@
-resource "aws_instance" "demo_ec2" {
-  ami = "ami-04e35eeae7a7c5883"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "demo_prod_ec2"
-  }
+module "demo_prod_ec2" {
+  source = "../modules/ec2"
 }
