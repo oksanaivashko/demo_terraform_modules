09842f92b45ee8dfe888319e7f6fa2d0037d9325
oksanaivashko <oksivashko13@gmail.com>
2023-08-16 17:08:15 -0500

    added vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,10 +11,10 @@ resource "aws_subnet" "demo_vpc_subnets" {
   map_public_ip_on_launch = true
 }
 
-output "vpc_id" {
-  value = module.demo_vpc.vpc_id
-}
-
-output "subnet_ids" {
-  value = module.demo_vpc.subnet_ids
-}
+#output "vpc_id" {
+#  value = module.demo_vpc.vpc_id
+#}
+#
+#output "subnet_ids" {
+#  value = module.demo_vpc.subnet_ids
+#}
