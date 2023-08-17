66fd3b5cd654d46d68732b927f5c93fe8311fa9a
oksanaivashko <oksivashko13@gmail.com>
2023-08-16 21:08:04 -0500

    tesitng ec2 with count

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,12 @@
 # --- Terraform module to create ec2 Insance ---
 
-#module "demo_ec2_dev" {
-#  source = "../../modules/ec2"
-#
+
+module "demo_ec2_dev" {
+  count = 5
+  source = "../../modules/ec2"
+}
+
+
 #  ami = var.ami
 #  ec2_type = var.ec2_type
 #  ec2_name = var.ec2_name
@@ -17,9 +21,9 @@
 
 # --- Terraform module to vpc ---
 
-module "demo_dev_vpc" {
-  source = "../../modules/vpc"
-  cidr_block    = "10.0.0.0/16"
-  demo_vpc_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
-  demo_vpc_az   = "us-west-2a"
-}
+#module "demo_dev_vpc" {
+#  source = "../../modules/vpc"
+#  cidr_block    = "10.0.0.0/16"
+#  demo_vpc_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+#  demo_vpc_az   = "us-west-2a"
+#}
